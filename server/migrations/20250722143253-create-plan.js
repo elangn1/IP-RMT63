cb3a2d7684798ec23f3e3cc4d576814b577b57f6
elangn1 <elangnoor01@gmail.com>
2025-07-22 22:05:48 +0700

    feat(db): add plan migration

---
+++ server/migrations/20250722143253-create-plan.js
@@ -0,0 +1,39 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Plans", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Users",
+          key: "id"
+        },
+        onDelete: "CASCADE"
+      },
+      judulBelajar: {
+        type: Sequelize.STRING
+      },
+      aiFeedback: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Plans");
+  }
+};
