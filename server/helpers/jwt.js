50ad27afd3eecb56cc4133743c5b7eb0dd122238
elangn1 <elangnoor01@gmail.com>
2025-07-25 09:34:39 +0700

    refactor: rename secret key

--- server/helpers/jwt.js
+++ server/helpers/jwt.js
@@ -1,15 +1,15 @@
 const jwt = require("jsonwebtoken");
-const SECRET_KEY = process.env.SECRET_KEY;
+const JWT_SECRET = process.env.JWT_SECRET;
 // console.log("Environment variables check:");
 // console.log("SECRET_KEY exists:", !!process.env.SECRET_KEY);
 // console.log("SECRET_KEY length:", process.env.SECRET_KEY ? process.env.SECRET_KEY.length : 0);
 
 const signToken = (payload) => {
-  return jwt.sign(payload, SECRET_KEY);
+  return jwt.sign(payload, JWT_SECRET);
 };
 
 const verifyToken = (token) => {
-  return jwt.verify(token, SECRET_KEY);
+  return jwt.verify(token, JWT_SECRET);
 };
 
 module.exports = {
