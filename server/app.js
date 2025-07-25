ddb6c657a79d201f27877a47ce888596166b920e
elangn1 <elangnoor01@gmail.com>
2025-07-25 08:41:33 +0700

    fix: route user

--- server/app.js
+++ server/app.js
@@ -13,8 +13,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to the User API" });
+});
+
 // user authentication routes
-app.use("/", userRoutes);
+app.use("/users", userRoutes);
 
 // plan routes
 app.use("/plans", planRoutes);
